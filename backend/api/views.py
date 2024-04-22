9e47ed7a5fbacb69943925bceb29ebb297bb88f4
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2024-04-22 01:56:10 -0600

    0:32

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,5 +7,5 @@ from .serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = User
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
